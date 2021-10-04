b592e23645b00c7f4d723ef1f76e45d3eebdbd03
wyneyusnaeni2014 <wina.wyi@bsi.ac.id>
2021-10-04 10:41:30 +0700

    Masukan Awal projek

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php 
+class contoh1 extends CI_Controller 
+{ 
+    public function index() 
+    { echo "<h1>Perkenalkan</h1>"; 
+        echo"Nama saya Wina Yusnaeni Saya 
+        tingga di daerah Ciputat olah raga yang saya 
+        sukai adalah Bulutangkis"; 
+    } 
+}`
